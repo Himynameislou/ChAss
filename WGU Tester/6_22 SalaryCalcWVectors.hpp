fc15648a0152966006ff849bf9dd0b9cae17b949
Luis Vegerano <louvwebdev@gmail.com>
2020-11-13 14:45:26 -0600

    Initial Commit

---
+++ WGU Tester/6_22 SalaryCalcWVectors.hpp
@@ -0,0 +1,13 @@
+//
+//  6_22 SalaryCalcWVectors.hpp
+//  WGU Tester
+//
+//  Created by Luis Vegerano on 11/13/20.
+//
+
+#ifndef __22_SalaryCalcWVectors_hpp
+#define __22_SalaryCalcWVectors_hpp
+
+#include <stdio.h>
+
+#endif /* __22_SalaryCalcWVectors_hpp */

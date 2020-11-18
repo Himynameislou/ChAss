59bcf10a0594480d3fe0b53120f6494e9bf9eade
Luis Vegerano <louvwebdev@gmail.com>
2020-11-18 10:33:51 -0600

    7.5.2 Function w/ loop; Shampoo

---
+++ WGU Tester/752 Function w Loop Shampoo.cpp
@@ -0,0 +1,32 @@
+//
+//  752 Function w Loop Shampoo.cpp
+//  WGU Tester
+//
+//  Created by Luis Vegerano on 11/18/20.
+//
+
+#include <iostream>
+using namespace std;
+
+void PrintShampooInstructions(int numCycles){
+
+	if(numCycles < 1){
+		cout << "Too few." << endl;
+	}
+	else if(numCycles > 4){
+		cout << "Too many." << endl;
+	}
+	else {
+		for (int i = 1; i <= numCycles; ++i) {
+			cout << i << ": Lather and rinse." << endl;
+		}
+		cout << "Done." << endl;
+	}
+	//return;
+}
+
+int shampoo() {
+   PrintShampooInstructions(2);
+
+   return 0;
+}

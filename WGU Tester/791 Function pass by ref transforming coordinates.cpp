9834f22303bf7d8bf41b96bdefdc1d5a8c7a56d5
Luis Vegerano <louvwebdev@gmail.com>
2020-11-19 12:14:48 -0600

    7.9.1: Function pass by reference: Transforming coordinates.

---
+++ WGU Tester/791 Function pass by ref transforming coordinates.cpp
@@ -0,0 +1,29 @@
+//
+//  791 Function pass by ref transforming coordinates.cpp
+//  WGU Tester
+//Define a function CoordTransform() that transforms its first two input parameters xVal and yVal into two output parameters xValNew and yValNew. The function returns void. The transformation is new = (old + 1) * 2. Ex: If xVal = 3 and yVal = 4, then xValNew is 8 and yValNew is 10.
+//  Created by Luis Vegerano on 11/19/20.
+//
+
+#include "791 Function pass by ref transforming coordinates.hpp"
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+void CoordTransform(int xVal, int yVal, int& xValNew, int& yValNew){
+    xValNew = (xVal + 1) * 2;
+    yValNew = (yVal + 1) * 2;
+    
+    return;
+}
+
+int trans() {
+    int xValNew = 0;
+    int yValNew = 0;
+
+    CoordTransform(3, 4, xValNew, yValNew);
+    cout << "(3, 4) becomes " << "(" << xValNew << ", " << yValNew << ")" << endl;
+   
+   
+   return 0;
+}
---
+++ WGU Tester/791 Function pass by ref transforming coordinates.hpp
@@ -0,0 +1,13 @@
+//
+//  791 Function pass by ref transforming coordinates.hpp
+//  WGU Tester
+//
+//  Created by Luis Vegerano on 11/19/20.
+//
+
+#ifndef _91_Function_pass_by_ref_transforming_coordinates_hpp
+#define _91_Function_pass_by_ref_transforming_coordinates_hpp
+
+#include <stdio.h>
+
+#endif /* _91_Function_pass_by_ref_transforming_coordinates_hpp */

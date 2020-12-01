036567c1c4ac51c6ec921d84f5e06b5d74b5e4ad
Luis Vegerano <louvwebdev@gmail.com>
2020-12-01 10:58:20 -0600

    Initial Push wO solution

---
+++ WGU Tester/8 2 3 Basic Class Definition.cpp
@@ -0,0 +1,43 @@
+//
+//  8 2 3 Basic Class Definition.cpp
+//  WGU Tester
+//
+//  Created by Luis Vegerano on 12/1/20.
+//
+
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+class DogLicense{
+   public:
+      void   SetYear(int yearRegistered);
+      void   CreateLicenseNum(int customID);
+      int    GetLicenseNum() const;
+   private:
+      int    licenseYear;
+      int    licenseNum;
+};
+
+void DogLicense::SetYear(int yearRegistered) {
+   licenseYear = yearRegistered;
+   return;
+}
+
+// FIXME: Write CreateLicenseNum()
+
+/* Your solution goes here  */
+
+int DogLicense::GetLicenseNum() const {
+   return licenseNum;
+}
+
+int main() {
+   DogLicense dog1;
+
+   dog1.SetYear(2014);
+   dog1.CreateLicenseNum(777);
+   cout << "Dog license: " << dog1.GetLicenseNum() << endl;
+
+   return 0;
+}

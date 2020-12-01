ff983bd388e6b03835f03bb9d6e836c2bdc0a055
Luis Vegerano <louvwebdev@gmail.com>
2020-12-01 10:55:59 -0600

    8.2.2: Basic class use. Print person1's kids, apply the IncNumKids() function, and print again, outputting text as below. End each line with newline. Sample output for below program: Kids: 3 New baby, kids now: 4

---
+++ WGU Tester/8 2 2 Basic Class Use.cpp
@@ -0,0 +1,48 @@
+//
+//  8 2 2 Basic Class Use.cpp
+//  WGU Tester
+//
+//  Created by Luis Vegerano on 12/1/20.
+//
+
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+class PersonInfo {
+   public:
+      void   SetNumKids(int personsKids);
+      void   IncNumKids();
+      int    GetNumKids() const;
+   private:
+      int    numKids;
+};
+
+void PersonInfo::SetNumKids(int personsKids) {
+   numKids = personsKids;
+   return;
+}
+
+void PersonInfo::IncNumKids() {
+   numKids = numKids + 1;
+   return;
+}
+
+int PersonInfo::GetNumKids() const {
+   return numKids;
+}
+
+int main() {
+   PersonInfo person1;
+
+   person1.SetNumKids(3);
+
+   /* Your solution goes here  */
+    cout << "Kids: " << person1.GetNumKids() << endl;
+       
+    person1.IncNumKids();
+       
+    cout << "New baby, kids now: " << person1.GetNumKids() <<  endl;
+
+   return 0;
+}

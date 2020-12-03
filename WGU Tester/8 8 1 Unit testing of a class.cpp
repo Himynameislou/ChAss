2fc751ef0e670d51af5905db9548409bef014064
Luis Vegerano <louvwebdev@gmail.com>
2020-12-03 11:08:10 -0600

    Initial problem w/o solution.

---
+++ WGU Tester/8 8 1 Unit testing of a class.cpp
@@ -0,0 +1,53 @@
+//
+//  8 8 1 Unit testing of a class.cpp
+//  WGU Tester
+//
+//  Created by Luis Vegerano on 12/3/20.
+//
+
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+class InventoryTag {
+public:
+   InventoryTag();
+   int getQuantityRemaining() const;
+   void addInventory(int numItems);
+
+private:
+   int quantityRemaining;
+};
+
+InventoryTag::InventoryTag() {
+    quantityRemaining = 0;
+}
+
+int InventoryTag::getQuantityRemaining() const {
+   return quantityRemaining;
+}
+
+void InventoryTag::addInventory(int numItems) {
+   if (numItems > 10) {
+      quantityRemaining = quantityRemaining + numItems;
+   }
+}
+
+int main() {
+   InventoryTag redSweater;
+   int sweaterShipment = 0;
+   int sweaterInventoryBefore = 0;
+
+   sweaterInventoryBefore = redSweater.getQuantityRemaining();
+   sweaterShipment = 25;
+
+   cout << "Beginning tests." << endl;
+
+   // FIXME add unit test for addInventory
+
+   /* Your solution goes here  */
+
+   cout << "Tests complete." << endl;
+
+   return 0;
+}

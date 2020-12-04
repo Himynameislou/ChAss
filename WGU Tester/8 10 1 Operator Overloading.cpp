b79b686afb8bd42718ae086291664c2f90fc47ed
Luis Vegerano <louvwebdev@gmail.com>
2020-12-04 15:34:01 -0600

    8.10.1: Operator overloading. Overload the + operator as indicated. Sample output for the given program: First vacation: Days: 7, People: 3 Second vacation: Days: 12, People: 3

--- WGU Tester/8 10 1 Operator Overloading.cpp
+++ WGU Tester/8 10 1 Operator Overloading.cpp
@@ -34,6 +34,11 @@ void FamilyVacation::SetNumPeople(int peopleCount) {
 //        which adds 5 to numDays, while just copying numPeople.
 
 /* Your solution goes here  */
+FamilyVacation FamilyVacation::operator+(int rhsNum){
+    FamilyVacation newVacation;
+    newVacation.numDays = numDays + rhsNum;
+    return newVacation;
+}
 
 void FamilyVacation::Print() const {
    cout << "Days: " << numDays << ", People: " << numPeople << endl;

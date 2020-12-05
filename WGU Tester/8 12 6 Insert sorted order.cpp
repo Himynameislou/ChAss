ffe4deb298f010cb9463258babdb7ac18aa880b4
Luis Vegerano <louvwebdev@gmail.com>
2020-12-05 11:30:33 -0600

    initial commit

---
+++ WGU Tester/8 12 6 Insert sorted order.cpp
@@ -0,0 +1,49 @@
+//
+//  8 12 6 Insert sorted order.cpp
+//  WGU Tester
+//
+//  Created by Luis Vegerano on 12/5/20.
+//
+
+#include <stdio.h>
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void numsInsert(vector<int>& numsList, int newNum) {
+   unsigned int i = 0;
+   for (i = 0; i < numsList.size(); ++i) {
+      if (newNum < numsList.at(i)) {
+         // FIXME: insert newNum at element i
+         break; // Exits the for loop
+      }
+   }
+
+   // FIXME: change so executes if higher number NOT found
+   // Change "true" to "i == ??" (determine what ?? should be)
+   if (true) { // No higher number was found, so append to end
+      numsList.push_back(newNum);
+
+   }
+   return;
+}
+
+void numsPrint(const vector<int>& numsList) {
+   unsigned int i = 0;
+   for (i = 0; i < numsList.size(); ++i) {
+      cout << " " << numsList.at(i) << endl;
+   }
+   return;
+}
+
+int main() {
+   vector<int> numsList;
+   numsInsert(numsList, 55);
+   numsInsert(numsList, 4);
+   numsInsert(numsList, 250);
+   numsInsert(numsList, 19);
+   numsPrint (numsList);
+
+   return 0;
+}

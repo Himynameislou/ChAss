4327a62ad7903596e490ede638a17fe01b408d58
Luis Vegerano <louvwebdev@gmail.com>
2020-12-09 11:32:01 -0600

    initial commit

---
+++ WGU Tester/9 2 1 printing w pointers.cpp
@@ -0,0 +1,19 @@
+//
+//  9 2 1 printing w pointers.cpp
+//  WGU Tester
+//
+//  Created by Luis Vegerano on 12/9/20.
+//
+
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+int main() {
+   int* numItemsPtr = nullptr;
+   int numItems = 99;
+
+   /* Your solution goes here  */
+
+   return 0;
+}

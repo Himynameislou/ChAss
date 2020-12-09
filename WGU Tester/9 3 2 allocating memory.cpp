5731b92df73da76300b0d0dc313683d9c54aaaad
Luis Vegerano <louvwebdev@gmail.com>
2020-12-09 13:14:37 -0600

    initial commit

---
+++ WGU Tester/9 3 2 allocating memory.cpp
@@ -0,0 +1,23 @@
+//
+//  9 3 2 allocating memory.cpp
+//  WGU Tester
+//
+//  Created by Luis Vegerano on 12/9/20.
+//
+
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+int main() {
+   double* houseHeight = nullptr;
+
+   /* Your solution goes here  */
+
+   *houseHeight = 23;
+   cout << "houseHeight is " << *houseHeight;
+
+   delete houseHeight;
+
+   return 0;
+}

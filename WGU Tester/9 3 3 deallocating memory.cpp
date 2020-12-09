32a61fe396c0c96873901018e7aa438d21228913
Luis Vegerano <louvwebdev@gmail.com>
2020-12-09 13:30:51 -0600

    Initial commit:
    9.3.3: Deallocating memory
    Deallocate memory for kitchenPaint using the delete operator.

---
+++ WGU Tester/9 3 3 deallocating memory.cpp
@@ -0,0 +1,32 @@
+//
+//  9 3 3 deallocating memory.cpp
+//  WGU Tester
+//
+//  Created by Luis Vegerano on 12/9/20.
+//
+
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+class PaintContainer {
+   public:
+      ~PaintContainer();
+      double gallonPaint;
+};
+
+PaintContainer::~PaintContainer() { // Covered in section on Destructors.
+   cout << "PaintContainer deallocated." << endl;
+   return;
+}
+
+int main() {
+   PaintContainer* kitchenPaint;
+
+   kitchenPaint = new PaintContainer;
+   kitchenPaint->gallonPaint = 26.3;
+
+   /* Your solution goes here  */
+
+   return 0;
+}

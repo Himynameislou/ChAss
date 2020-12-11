3a3d2e3266628c9b2710ed8934e0b746246b4b1f
Luis Vegerano <louvwebdev@gmail.com>
2020-12-11 15:47:39 -0600

    Initial Commit
    9.4.1: Find char in C string
    Assign a pointer to any instance of searchChar in personName to searchResult.

---
+++ WGU Tester/9 4 1 find char in C string.cpp
@@ -0,0 +1,30 @@
+//
+//  9 4 1 find char in C string.cpp
+//  WGU Tester
+//
+//  Created by Luis Vegerano on 12/11/20.
+//
+
+#include <stdio.h>
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main() {
+   char personName[100] = "Albert Johnson";
+   char searchChar = 'J';
+   char* searchResult = nullptr;
+
+   /* Your solution goes here  */
+    
+
+
+   if (searchResult != nullptr) {
+      cout << "Character found." << endl;
+   }
+   else {
+      cout << "Character not found." << endl;
+   }
+
+   return 0;
+}

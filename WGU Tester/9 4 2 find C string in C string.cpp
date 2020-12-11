cb10d31fbf6046379c0badf9b9d8a3b0447aca69
Luis Vegerano <louvwebdev@gmail.com>
2020-12-11 15:49:56 -0600

    Initial commit
    9.4.2: Find C string in C string.
    Assign the first instance of The in movieTitle to movieResult.

---
+++ WGU Tester/9 4 2 find C string in C string.cpp
@@ -0,0 +1,28 @@
+//
+//  9 4 2 find C string in C string.cpp
+//  WGU Tester
+//
+//  Created by Luis Vegerano on 12/11/20.
+//
+
+#include <stdio.h>
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main() {
+   char movieTitle[100] = "The Lion King";
+   char* movieResult = nullptr;
+
+   /* Your solution goes here  */
+
+   cout << "Movie title contains The? ";
+   if (movieResult != nullptr) {
+      cout << "Yes." << endl;
+   }
+   else {
+      cout << "No." << endl;
+   }
+
+   return 0;
+}
